6ac3aaa954dbb4c942fd2e29b3a80fac18bfa08e
willstorrs <34176205+willstorrs@users.noreply.github.com>
2023-05-29 15:06:24 -0400

    Edits

---
+++ app.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Company Information</title>
+  <link rel="stylesheet" href="app.css">
+</head>
+<body>
+  <header>
+    <h1 id="company-name"></h1>
+  </header>
+  
+  <main id="company-info">
+    <!-- Company information will be inserted here -->
+  </main>
+  
+  <footer>
+    A Will Storrs Production
+  </footer>
+  
+  <a href="index.html" id="home-link">Back to Home</a>
+
+  <script src="company.js"></script>
+</body>
+</html>
