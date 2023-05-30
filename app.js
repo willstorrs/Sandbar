338a7dc8f4ad8f835a115e08f07db1eb287bc00b
willstorrs <34176205+willstorrs@users.noreply.github.com>
2023-05-29 21:14:12 -0400

    v2.8

--- app.js
+++ app.js
@@ -5,6 +5,32 @@ let companyLink = document.querySelector("#company-link");
 let homeLink = document.querySelector("#home-link");
 let companyNav = document.querySelector("#company-nav");
 let companyContent = document.querySelector("#company-content");
+let dataList = document.querySelector("#companies");
+
+// function to fetch companies data
+function fetchCompanies() {
+  return fetch('companies.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("HTTP error " + response.status);
+      }
+      return response.json();
+    });
+}
+
+// function to populate datalist for search input
+function populateDataList(data) {
+  data.forEach(item => {
+    const option = document.createElement('option');
+    option.value = item.ticker;
+    dataList.appendChild(option);
+  });
+}
+
+// Fetch company data from JSON file
+fetchCompanies()
+  .then(data => populateDataList(data))
+  .catch(error => console.log(error));
 
 // Search form submit event
 searchForm.addEventListener("submit", function (e) {
@@ -36,50 +62,6 @@ function displayCompanyDetails() {
   // then populate the #company-nav and #company-content with fetched data
 }
 
-// Here you can add more JavaScript logic to your application
-// For instance, you might want to fetch some initial data when the page loads
-
 window.addEventListener('load', (event) => {
   console.log('The page has fully loaded');
-  // you can call any function that fetches data here.
 });
-// Fetch company data from JSON file
-fetch('companies.json')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("HTTP error " + response.status);
-        }
-        // If response is OK, return the JSON data
-        return response.json();
-    })
-    .then(data => {
-        // Get datalist element
-        const datalist = document.getElementById('companies');
-        
-        // Populate datalist with company names and tickers
-        data.forEach(item => {
-            // Add company name option
-            const optionName = document.createElement('option');
-            optionName.value = item.name;
-            datalist.appendChild(optionName);
-
-            // Add ticker option
-            const optionTicker = document.createElement('option');
-            optionTicker.value = item.ticker;
-            datalist.appendChild(optionTicker);
-        });
-
-        // Add event listener to form submission
-        document.getElementById('search-form').addEventListener('submit', function(e) {
-            e.preventDefault();
-            const searchQuery = document.getElementById('search-input').value;
-            const company = data.find(item => item.name.toLowerCase() === searchQuery.toLowerCase() || item.ticker.toLowerCase() === searchQuery.toLowerCase());
-            if (company) {
-                localStorage.setItem('selectedCompany', JSON.stringify(company));
-                window.location.href = 'company.html';
-            }
-        });
-    })
-    .catch(function(error) {
-        console.log(error);
-    });
