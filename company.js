51301948722590e32b8fb432e76fc89015f02665
willstorrs <34176205+willstorrs@users.noreply.github.com>
2023-05-29 22:13:24 -0400

    v2.12

--- company.js
+++ company.js
@@ -1,56 +1,41 @@
-function fetchCompanies() {
-  return fetch('companies.json')
+// Fetching details about a company
+function fetchCompanyDetails(company) {
+    return fetch(`companyDetails/${company}.json`)
       .then(response => {
-          if (!response.ok) {
-              throw new Error("HTTP error " + response.status);
-          }
-          return response.json();
+        if (!response.ok) {
+          throw new Error("HTTP error " + response.status);
+        }
+        return response.json();
       });
-}
-
-fetchCompanies()
-  .then(data => {
-      let companyData = JSON.parse(localStorage.getItem('selectedCompany'));
-
-      if (companyData) {
-          const company = data.find(item => item.CIKNOZ === companyData.CIKNOZ);
-
-          if (company) {
-              document.getElementById('company-name').textContent = company.name;
-
-              const info = document.getElementById('company-info');
-              
-              Object.keys(company).forEach(key => {
-                  if (key !== 'name') {
-                      const p = document.createElement('p');
-                      p.textContent = `${key}: ${company[key]}`;
-                      info.appendChild(p);
-                  }
-              });
-          } else {
-              window.location.href = 'index.html';
-          }
-      }
-
-      localStorage.removeItem('selectedCompany');
-  })
-  .catch(error => {
-      console.log(error);
-      alert('An error occurred while fetching companies data. Please try again later.');
-  });
-
-// Update the page title and header on page load
-window.onload = () => {
-  let companyData = JSON.parse(localStorage.getItem('selectedCompany'));
-
-  if (companyData) {
-      document.title = `${companyData.name} - Company Info`;
-
-      let header = document.querySelector('header h1');
-      if (header) {
-          header.textContent = `${companyData.name} - Company Info`;
-      }
-  } else {
-      window.location.href = 'index.html';
   }
-};
+  
+  // Update the page title and header on page load
+  window.onload = () => {
+    // First, we retrieve the company name from local storage.
+    let companyName = localStorage.getItem('selectedCompany');
+  
+    // If a company name was stored...
+    if (companyName) {
+      // Update the page title and header.
+      document.title = companyName;
+      document.querySelector('#company-name').textContent = companyName;
+  
+      // Then, fetch the company details.
+      fetchCompanyDetails(companyName)
+        .then(companyDetails => {
+          // Assuming your companyDetails is an object with a 'profile' property
+          if (companyDetails && companyDetails.profile) {
+            document.querySelector('#company-info').textContent = companyDetails.profile;
+          }
+        })
+        .catch(error => console.log(error));
+  
+      // We can remove the company name from local storage now that we're done with it.
+      localStorage.removeItem('selectedCompany');
+    }
+    else {
+      // If no company name was stored, redirect back to the search page.
+      window.location.href = 'index.html';
+    }
+  };
+  
