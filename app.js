b0b450660315f359afb87af63845ceffdec1e261
willstorrs <34176205+willstorrs@users.noreply.github.com>
2023-05-29 20:59:56 -0400

    Revert "v2.5"
    This reverts commit b2e06d5e07b0795cc9529135684c3b2642f07bc8.

--- app.js
+++ app.js
@@ -43,3 +43,43 @@ window.addEventListener('load', (event) => {
   console.log('The page has fully loaded');
   // you can call any function that fetches data here.
 });
+// Fetch company data from JSON file
+fetch('companies.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("HTTP error " + response.status);
+        }
+        // If response is OK, return the JSON data
+        return response.json();
+    })
+    .then(data => {
+        // Get datalist element
+        const datalist = document.getElementById('companies');
+        
+        // Populate datalist with company names and tickers
+        data.forEach(item => {
+            // Add company name option
+            const optionName = document.createElement('option');
+            optionName.value = item.name;
+            datalist.appendChild(optionName);
+
+            // Add ticker option
+            const optionTicker = document.createElement('option');
+            optionTicker.value = item.ticker;
+            datalist.appendChild(optionTicker);
+        });
+
+        // Add event listener to form submission
+        document.getElementById('search-form').addEventListener('submit', function(e) {
+            e.preventDefault();
+            const searchQuery = document.getElementById('search-input').value;
+            const company = data.find(item => item.name.toLowerCase() === searchQuery.toLowerCase() || item.ticker.toLowerCase() === searchQuery.toLowerCase());
+            if (company) {
+                localStorage.setItem('selectedCompany', JSON.stringify(company));
+                window.location.href = 'company.html';
+            }
+        });
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
